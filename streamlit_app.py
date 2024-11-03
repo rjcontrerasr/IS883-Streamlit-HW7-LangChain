e4ac274e7037956d948b89286d1b62af51771df8
rjcontrerasr <156374431+rjcontrerasr@users.noreply.github.com>
2024-11-03 00:57:46 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ from openai import OpenAI
 import os
 
 ### Load your API Key
-my_secret_key= st.secrets["MyOpenAIKey"]
+my_secret_key= st.secrets("MyOpenAIKey")
 os.environ["OPENAI_API_KEY"] = my_secret_key
 
 st.title("Share with us your experience of the latest trip")
